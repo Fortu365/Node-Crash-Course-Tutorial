1efd63d7b52e4bd6b096bf022145bf9466dcabc6
Fortu365 <fortubelogift@gmail.com>
2025-07-22 16:07:42 +0200

    I made changes to the uploaded files and added new ejs files to them and removed the js files

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 const express = require('express');
 const { dirname } = require('path');
+const { title } = require('process');
 
 // Express App 
 const app = express(); // Invoke the funx to create the instance of express app
 
 // Regiter View Engine 
+app.set('view engine', 'ejs');  // Tells the browser that ejs will be using the templates 
 
 
 // Listen for requests 
 app.listen(3000);  // Returns server instance 
 
-app.get('/', (req, res) => { /* Method used when you want to get the response, 
-                        1. What path of the url do you want to listen to
-                         2. Funx that takes the req and res objects  */
-    //res.send('<p> Home Page </p>');
-    res.sendFile('./views/index.html', { root: __dirname }); /*Shows us the current directory that we're currently in,on this file 
-                                                        Means: It will firstly check the curr directory, then go to the files inside it*/
+app.get('/', (req, res) => { 
+    const blogs = [
+        {title: 'Yoshi finds no error', snippet: 'This is the code snippet for Yoshi'},
+        {title: 'Fortunate', snippet: 'This is the code snippet for Fortunate'},
+        {title: 'Mahlodi', snippet: 'This is the code snippet for Yoshi'},
+    ];
+    res.render('index', { title: 'Home'});
+    
 }); 
 
-app.get('/about', (req, res) => { /* The same as the above  */
-    //res.send('<p> About Page </p>');
-    res.sendFile('./views/about.html', { root: __dirname }); // Same as the above, but with different pages 
+app.get('/about', (req, res) => { 
+   res.render('about', { title: 'About'});
 });
 
-app.get('/other', (req, res) => { /* The same as the above  */
-    //res.send('<p> About Page </p>');
-    res.sendFile('./views/other.html', { root: __dirname }); // Same as the above, but with different pages 
+app.get('/other', (req, res) => { 
+    res.render('other', { title: 'Other'});
 });
 
-// Using Redirects InExpress, its a short cut as you don't have to set the status code, and end the res 
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog'});
 });
-
 
 /* Dealing with 404 page in Express. Works similar to the switch, as the deafult
     Always written at the bottom, cause it will run to the defualt[value] while the  
     page you're looking for exists*/
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404'});
 });
 
